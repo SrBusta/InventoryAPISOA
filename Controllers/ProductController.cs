c90c10851ab0ad16eb07fec3705fc9c1ed6f419e
SrBusta <87667489+SrBusta@users.noreply.github.com>
2023-06-27 11:50:45 -0500

    Initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace InventoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private string _connection = @"Server=localhost; Database=inventory; Uid=root; Pwd=admin";
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Models.Product> lst = null;
+            using (var db=new MySqlConnection(_connection))
+            {
+            var sql = "select id,description from product";
+                lst = db.Query<Models.Product>(sql);
+            }
+
+            return Ok(lst);
+        }
+    }
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InsertProductController : ControllerBase
+    {
+        private string _connection = @"Server=localhost; Database=inventory; Uid=root; Pwd=admin";
+
+        [HttpPost]
+        public IActionResult Insert(Models.Product model)
+        {
+            int result = 0;
+            using (var db = new MySqlConnection(_connection))
+            {
+                var sql = "insert into product(id,description,stock)"+
+                    " values(@id,@description,@stock)";
+                result = db.Execute(sql, model);
+              
+            }
+            return Ok(result);
+        }
+    }
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryAPI.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public int stock { get; set; }
+            
+    }
+}
